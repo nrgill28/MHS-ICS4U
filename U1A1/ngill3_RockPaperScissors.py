a32098a242b2daf3e615c351b8d7563755bb9471
Nathan Gill <nrgill28@gmail.com>
2020-02-18 10:08:49 -0500

    Finished Unit 1 Assignment a

---
+++ U1A1/ngill3_RockPaperScissors.py
@@ -0,0 +1,9 @@
+"""
+Author . . : Nathan Gill
+Course . . : ICS4U
+File . . . : ngill3_RockPaperScissors.py
+Description:
+    
+History . .:
+    2/18/2020 - Created File
+"""
---
+++ U1A1/ngill3_TheGreatest.py
@@ -0,0 +1,9 @@
+"""
+Author . . : Nathan Gill
+Course . . : ICS4U
+File . . . : ngill3_TheGreatest.py
+Description:
+    
+History . .:
+    2/18/2020 - Created File
+"""
