eee51604a33cd8b4c4f926d15452aed8d7c6c1c0
Nathan Gill <nrgill28@gmail.com>
2020-05-27 17:04:29 -0400

    Comments everywhere

--- U6T1/ngill3_U6T1.py
+++ U6T1/ngill3_U6T1.py
@@ -1,10 +1,20 @@
 from typing import List, Any, Callable
 
+try:
+    from U5T3.ngill3_utils import pp_table
+except ModuleNotFoundError:
+    import sys
+    print("This file depends on 'U5T3.ngill3_utils.py'!"
+          " Try downloading the entire repo from https://github.com/nrgill28/MHS-ICS4U/"
+          " and run this file from there instead.")
+    sys.exit(0)
+
 """
 This file just has the algorithms. Open the tests file for where they're used.
 """
 
 
+# Extremely simple linear search algorithm. I don't think anything needs to be explained here.
 def linear_search(lst: List[Any], item: Any) -> bool:
     for i in lst:
         if i == item:
@@ -12,6 +22,7 @@ def linear_search(lst: List[Any], item: Any) -> bool:
     return False
 
 
+# Binary search algorithm. This requires the list to be sorted in advance, but is generally quicker than linear search
 def binary_search(lst: List[int], item: int) -> bool:
     sorted(lst)
     start, end = 0, len(lst) - 1
@@ -27,29 +38,53 @@ def binary_search(lst: List[int], item: int) -> bool:
     return False
 
 
-def bubble_sort(lst: List[Any], sort_method: Callable[[Any], int] = lambda x: x) -> List[Any]:
+# Bubble sort algorithm
+def bubble_sort(lst: List[Any], sort_method: Callable[[Any], int] = lambda x: x, print_passes: bool = False) -> List[Any]:
+    # Make a copy of the list, and a new list to hold the data at the beginning of each pass
     ret = lst.copy()
+    passes = []
+
+    # Run the algorithm
     for num in range(len(ret) - 1, 0, -1):
+        passes.append(ret.copy())
         for i in range(num):
             if sort_method(ret[i]) > sort_method(ret[i + 1]):
                 foo = ret[i]
                 ret[i] = ret[i + 1]
                 ret[i + 1] = foo
+
+    # Once the algorithm is finished, add the last pass to the list and print it if we wanted
+    passes.append(ret.copy())
+    if print_passes: display_passes(passes)
+
+    # return the sorted list
     return ret
 
 
-def insertion_sort(lst: List[Any], sort_method: Callable[[Any], int] = lambda x: x) -> List[Any]:
+def insertion_sort(lst: List[Any], sort_method: Callable[[Any], int] = lambda x: x, print_passes: bool = False) -> List[Any]:
+    # Make a copy of the list, and a new list to hold the data at the beginning of each pass
     ret = lst.copy()
+    passes = []
+
+    # Run the algorithm
     for i in range(1, len(ret)):
+        passes.append(ret.copy())
         item = ret[i]
         j = i - 1
         while j >= 0 and sort_method(item) < sort_method(ret[j]):
             ret[j + 1] = ret[j]
             j -= 1
         ret[j + 1] = item
+
+    # Once the algorithm is finished, add the last pass to the list and print it if we wanted
+    passes.append(ret.copy())
+    if print_passes: display_passes(passes)
+
+    # Return the sorted list
     return ret
 
 
+# Quicksort algorithm
 def quick_sort(lst: List[int], sort_method: Callable[[Any], int] = lambda x: x) -> List[Any]:
     below, equal, above = [], [], []
     if len(lst) > 1:
@@ -63,5 +98,18 @@ def quick_sort(lst: List[int], sort_method: Callable[[Any], int] = lambda x: x) 
     else: return lst
 
 
+# This method is called if you wanted to display the state of each pass when sorting with Insertion or Bubble sort.
+# Basically it takes in a list of lists and prints it using my pp_table function that pretty prints a table.
+def display_passes(pass_list: List[List]):
+    headers = [f"Index {i}" for i in range(len(pass_list[0]))]
+    headers.insert(0, "Pass")
+    for i, row in enumerate(pass_list):
+        pass_list[i] = [str(x) for x in row]
+        pass_list[i].insert(0, str(i))
+    pass_list.insert(0, headers)
+    pp_table(pass_list)
+
+
+# Running this file directly does nothing.
 if __name__ == "__main__":
     print("Don't run this file directly! Run the other one instead!")
--- U6T2/ngill3_U6T2.py
+++ U6T2/ngill3_U6T2.py
@@ -10,13 +10,23 @@ History . .:
 
 import random
 import csv
+import sys
 
-# Import the menu function I made for the Car Inventory Task
-from U5T3.ngill3_utils import show_menu, get_user_input
+# Try to import my other files. This is so I don't have to write code twice!
+try:
+    # Import the menu function I made for the Car Inventory Task
+    from U5T3.ngill3_utils import show_menu, get_user_input
 
-# Import the search and sort functions from the last task
-from U6T1.ngill3_U6T1 import linear_search, binary_search, bubble_sort, insertion_sort, quick_sort
+    # Import the search and sort functions from the last task
+    from U6T1.ngill3_U6T1 import linear_search, binary_search, bubble_sort, insertion_sort
+except ModuleNotFoundError:
+    # If it can't find the other files, print a warning and exit.
+    print("This file depends on files from U5T3 and U6T1! "
+          "Download the entire repo at https://github.com/nrgill28/MHS-ICS4U/ "
+          "and run the same file from there (It'll be located in a folder with the same task name).")
+    sys.exit(0)
 
+# Yep here's all the data. Constant lists of each item category that can be used to randomly pick from
 FILE_LOC = "character.csv"
 SWORDS = ["Small Sword", "Medium Sword", "Large Sword"]
 AXES = ["Small Axe", "Medium Axe", "Large Axe"]
@@ -32,11 +42,15 @@ STATS = ["Strength", "Constitution", "Dexterity", "Intelligence", "Wisdom", "Cha
 FIELDNAMES = ["first_name", "last_name", "class", "race", "stats", "back_slot", "belt_slot", "right_hand", "left_hand",
               "pack"]
 
+# Here we combine a few of the lists to create a pool of available items to certain character attributes.
+# All weapons for example are valid in either the back or belt slots on the character, so we combine the Sword, Axe
+# and Bow lists into one here to pick from later
 SLOT_ITEMS = SWORDS + AXES + BOWS
 HAND_ITEMS = MISC_ITEMS + MAGICAL_ITEMS
 PACK_ITEMS = MISC_ITEMS + POTIONS + LOOT + HEALING_ITEMS + MAGICAL_ITEMS
 
 
+# This is a simple function to generate a character.
 def generate_character():
     return {
         "first_name": input("First Name: "),
@@ -52,6 +66,7 @@ def generate_character():
     }
 
 
+# This is a simple function to write the character to the CSV file.
 def write_character_to_file(character):
     with open(FILE_LOC, "w", newline='') as f:
         writer = csv.DictWriter(f, fieldnames=FIELDNAMES)
@@ -60,6 +75,8 @@ def write_character_to_file(character):
     print(f"\nCharacter saved as {FILE_LOC}")
 
 
+# This is a simple function to read the first character from the CSV file.
+# Technically the CSV format I use can support multiple characters, though I don't make use of it.
 def read_character_from_file():
     try:
         with open(FILE_LOC, 'r') as f:
@@ -71,12 +88,16 @@ def read_character_from_file():
             # Convert the list items back to lists
             character['pack'] = eval(character['pack'])
             character['stats'] = eval(character['stats'])
+
+            # Return the new Dict
             return character
     except FileNotFoundError:
+        # If the file isn't found, print a generic error message and return None
         print("Please generate a character before doing this.")
         return None
 
 
+# Display the stats for the loaded character.
 def display_character():
     character = read_character_from_file()
     print(f"Character: {character['first_name']} {character['last_name']}")
@@ -87,6 +108,7 @@ def display_character():
     print("Pack: " + (", ".join([x for x in character['pack'] if x != "Empty"])))
 
 
+# This function does a linear search through the saved character's pack to find an item
 def linear_search_pack():
     character = read_character_from_file()
     if character is None:
@@ -101,35 +123,59 @@ def linear_search_pack():
         print("No, the character's pack does not contain that item.")
 
 
+# This function does a binary search through the character's stats to find a specific value
 def binary_search_stats():
     character = read_character_from_file()
     if character is None:
         return
-    value = int(get_user_input("Enter the value to search for: ", lambda x: 1 <= int(x) <= 20))
+    value = int(get_user_input("Enter the value to search for (1-20): ", lambda x: 1 <= int(x) <= 20))
     if binary_search(character['stats'], value):
         print("Yes, one of the character's stats is this value")
     else:
         print("No, the character has no stats that are this value")
 
 
+# This function sorts the character's stats (with insertion sort), outputs the result and the end state of each pass
 def insertion_sort_stats():
+    # Make sure we have a saved character
     character = read_character_from_file()
     if character is None:
         return
-    # I coded the sort functions to accept list items and a callable that tells the algorithm which value to use for sorting
-    sorted_stats = insertion_sort(list(zip(character['stats'], STATS)), lambda x: x[0])
+
+    # This is my sort function. It takes 3 parameters:
+    #   The list
+    #   An anonymous function that describes which value is used for sorting when the input list is a list of objects or a list of lists
+    #   A boolean value if you want to print the results of each pass while it is sorting
+    # It then returns a sorted list with the same content.
+    # Here, I use the function list(zip()) to combine the character's stat values with the name of the
+    # stats, to keep them in the same order for easier displaying
+    sorted_stats = insertion_sort(list(zip(character['stats'], STATS)), lambda x: x[0], True)
+
+    # Then print the stats. Since the sorted list contains both the value and name, we can .join() it.
     print("Sorted stats:\n" + ", ".join(f"{n}: {s}" for s, n in sorted_stats))
 
 
+# This function sorts the character's stats (with bubble sort), outputs the result and the end state of each pass
 def bubble_sort_stats():
+    # Make sure we have a saved character
     character = read_character_from_file()
     if character is None:
         return
-    # I coded the sort functions to accept list items and a callable that tells the algorithm which value to use for sorting
-    sorted_stats = bubble_sort(list(zip(character['stats'], STATS)), lambda x: x[0])
+
+    # This is my sort function. It takes 3 parameters:
+    #   The list
+    #   An anonymous function that describes which value is used for sorting when the input list is a list of objects or a list of lists
+    #   A boolean value if you want to print the results of each pass while it is sorting
+    # It then returns a sorted list with the same content.
+    # Here, I use the function list(zip()) to combine the character's stat values with the name of the
+    # stats, to keep them in the same order for easier displaying
+    sorted_stats = bubble_sort(list(zip(character['stats'], STATS)), lambda x: x[0], True)
+
+    # Then print the stats. Since the sorted list contains both the value and name, we can .join() it.
     print("Sorted stats:\n" + ", ".join(f"{n}: {s}" for s, n in sorted_stats))
 
 
+# This binds the menu functions to text names
 MENU = [
     ("Create and save a new character", lambda: write_character_to_file(generate_character())),
     ("Display the saved character", display_character),
@@ -137,8 +183,10 @@ MENU = [
     ("Search the character's stats for a specific value (Binary search)", binary_search_stats),
     ("Sort the character's stats (Insertion sort)", insertion_sort_stats),
     ("Sort the character's stats (Bubble sort)", bubble_sort_stats),
+    ("Exit", lambda: sys.exit(0))
 ]
 
+# Forever, just call my show_menu function to show the menu. Exiting is done by using the exit menu option
 while True:
     show_menu("Menu", MENU)
     input("Press enter to continue.")
